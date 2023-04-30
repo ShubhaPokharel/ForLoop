4ad70fba31a1aac7eacc0b0311479ef9b8595270
ꜱʜᴜʙʜᴀ <93957744+ShubhaPokharel@users.noreply.github.com>
2023-04-30 15:11:01 -0400

    Add files via upload
    This is for loop practice!

---
+++ Test.java
@@ -0,0 +1,21 @@
+public class Test  {  
+public static void main(String args[]) {  
+int number=20;  
+System.out.println("List of even numbers from 1 to "+number+": ");
+System.out.println("This is even number");   
+for (int i=1; i<=number; i++)   {  
+//logic to check if the number is even or not  
+//if i%2 is equal to zero, the number is even  
+if (i%2==0){  
+System.out.println(i + " "); 
+}  
+ }
+ System.out.println("This is odd number");
+ System.out.println("List of odd numbers from 1 to "+number+": "); 
+  for(int s = 1;s<=20; s++){
+    if(s % 2!= 0){
+        System.out.println(s);
+    }
+  }
+   }  
+    }  
