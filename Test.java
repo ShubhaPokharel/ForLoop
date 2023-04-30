fcd1e61fbb030fb50cf622c1e6f761f435fd2943
ꜱʜᴜʙʜᴀ <93957744+ShubhaPokharel@users.noreply.github.com>
2023-04-30 15:12:25 -0400

    Update Test.java

--- Test.java
+++ Test.java
@@ -4,8 +4,7 @@ int number=20;
 System.out.println("List of even numbers from 1 to "+number+": ");
 System.out.println("This is even number");   
 for (int i=1; i<=number; i++)   {  
-//logic to check if the number is even or not  
-//if i%2 is equal to zero, the number is even  
+  
 if (i%2==0){  
 System.out.println(i + " "); 
 }  
